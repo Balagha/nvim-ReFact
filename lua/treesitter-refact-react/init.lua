31d193b7f2d5a729c72fb21ba02e9f7bc657de17
asifjoardar <mrsparrow04@gmail.com>
2022-08-25 19:41:43 +0600

    react code snippet added for new file

--- lua/treesitter-refact-react/init.lua
+++ lua/treesitter-refact-react/init.lua
@@ -2,13 +2,14 @@ local ts_utils = require("nvim-treesitter.ts_utils")
 
 local M = {}
 
+-- function
 local get_match_index = function(node)
   local nodeText = vim.treesitter.get_node_text(node, vim.api.nvim_get_current_buf())
   local matchIndexStart, matchIndexEnd = string.find(nodeText,"</")
-
   return matchIndexStart, matchIndexEnd
 end
 
+-- function
 local get_main_node = function()
 
   local node = ts_utils.get_node_at_cursor()
@@ -18,11 +19,11 @@ local get_main_node = function()
   end
 
   local root = ts_utils.get_root_for_node(node)
-
   local start_row = node:start()
   local parent = node:parent()
 
   matchIndexStart, matchIndexEnd = get_match_index(node)
+
   if(matchIndexStart == 1 and matchIndexEnd == 2) then
     node = parent
     parent = node:parent()
@@ -42,6 +43,30 @@ local get_main_node = function()
 
 end
 
+-- function
+local insert_code_into_new_file = function(file_name)
+
+  local node = get_main_node()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local start_row, start_col, end_row, end_col = node:range()
+  local text = vim.api.nvim_buf_get_text(bufnr, start_row, start_col, end_row, end_col, {})
+
+  local pre_code = "export function"..file_name.."({}) {\n  return (\n"
+  local post_code = "  );\n}"
+
+  local i = 1
+
+  local f = io.open(file_name..".js", "w")
+  f:write(pre_code)
+  while(text[i]~=nil) do
+    f:write(tostring(text[i]).."\n")
+    i = i + 1
+  end
+  f:write(post_code)
+  f:close()
+
+end
+
 M.select = function()
 
   local node = get_main_node()
@@ -52,22 +77,16 @@ end
 
 M.change = function()
 
+  local file_name = "dummy"
   local node = get_main_node()
   local bufnr = vim.api.nvim_get_current_buf()
   local start_row, start_col, end_row, end_col = node:range()
-  local replaced = 'abraka dabra'
-  local text = vim.api.nvim_buf_get_text(bufnr, start_row, start_col, end_row, end_col, {})
-  
-  local i=1
-  local f = io.open("abrakadabra.js", "a")
-  while(text[i]~=nil) do
-    f:write(tostring(text[i]).."\n")
-    i = i + 1
-  end
-  f:close()
+
+  local replaced = "<"..file_name.."/>"
+  insert_code_into_new_file(file_name)
 
   vim.api.nvim_buf_set_text(bufnr, start_row, start_col, end_row, end_col, { replaced })
-
+  
 end
 
 return M
